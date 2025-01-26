8bdf14f9b93cb43161896812496cf87c157263c9
MPPFinance <dev@mppfinance.xyz>
2025-01-26 09:07:57 +0300

    docs: update README with examples

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { MPPFinance } from './client'
 export { MPPFinanceMCP } from './mcp'
 export type { CardOptions, CardResult, SpendingRules, MPPFinanceConfig, CardEvent } from './types'
 // v0 - Wed Jan 15 05:14:24 MSK 2025
+// v6 - Sun Jan 26 09:07:57 MSK 2025
