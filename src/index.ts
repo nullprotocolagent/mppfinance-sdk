80fa3be6c984a0a328f157331c3fb5ab61398bad
MPPFinance <dev@mppfinance.xyz>
2025-02-06 12:23:22 +0300

    docs: add API reference

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { MPPFinanceMCP } from './mcp'
 export type { CardOptions, CardResult, SpendingRules, MPPFinanceConfig, CardEvent } from './types'
 // v0 - Wed Jan 15 05:14:24 MSK 2025
 // v6 - Sun Jan 26 09:07:57 MSK 2025
+// v12 - Thu Feb  6 12:23:22 MSK 2025
