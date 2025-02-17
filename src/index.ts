3baf0e966b53ab717576011512996bf41fcf4c16
MPPFinance <dev@mppfinance.xyz>
2025-02-17 11:08:52 +0300

    docs: add quickstart guide

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export type { CardOptions, CardResult, SpendingRules, MPPFinanceConfig, CardEven
 // v0 - Wed Jan 15 05:14:24 MSK 2025
 // v6 - Sun Jan 26 09:07:57 MSK 2025
 // v12 - Thu Feb  6 12:23:22 MSK 2025
+// v18 - Mon Feb 17 11:08:52 MSK 2025
