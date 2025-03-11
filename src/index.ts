e221d43d91d434faa7279ea90814868015168303
MPPFinance <dev@mppfinance.xyz>
2025-03-11 17:27:39 +0300

    fix: resolve ESM/CJS exports

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export type { CardOptions, CardResult, SpendingRules, MPPFinanceConfig, CardEven
 // v12 - Thu Feb  6 12:23:22 MSK 2025
 // v18 - Mon Feb 17 11:08:52 MSK 2025
 // v24 - Fri Feb 28 15:00:13 MSK 2025
+// v30 - Tue Mar 11 17:27:39 MSK 2025
